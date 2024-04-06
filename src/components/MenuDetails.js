b8e318adeaef1cfda0588f932df3749a53affef4
aishwaryaverma28 <aishwarya.verma28@gmail.com>
2024-04-06 17:01:23 +0530

    menu details

--- src/components/MenuDetails.js
+++ src/components/MenuDetails.js
@@ -1,43 +1,51 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { useFetch } from '../utils/hooks/useFetch';
-import { MENU_LIST_URL } from '../utils/constants';
-import { IMG_BASE_URL } from '../utils/constants';
-import discountLogo from '../assets/discount.png';
-import Shimmer from './Shimmer';
-import foodPlaceholder from '../assets/restaurant.png';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useFetch } from "../utils/hooks/useFetch";
+import { MENU_LIST_URL } from "../utils/constants";
+import { IMG_BASE_URL } from "../utils/constants";
+import discountLogo from "../assets/discount.png";
+import Shimmer from "./Shimmer";
+import foodPlaceholder from "../assets/restaurant.png";
 import styles from "./RestaurantCard.module.css";
-import "./MenuDetailsBanner.css"
-import { useDispatch } from 'react-redux';
-import { addItem } from '../utils/cartSlice';
+import "./MenuDetailsBanner.css";
+import { useDispatch } from "react-redux";
+import { addItem } from "../utils/cartSlice";
 
 function MenuDetails() {
   const { id } = useParams();
-  // const { data, isPending, error } = useFetch(MENU_LIST_URL + id); 
+  // const { data, isPending, error } = useFetch(MENU_LIST_URL + id);
 
-  const { data, isPending, error } = useFetch(`http://localhost:3001/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=19.159014&lng=72.9985686&submitAction=ENTER&restaurantId=${id}`);
+  const { data, isPending, error } = useFetch(
+    `http://localhost:3001/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=19.159014&lng=72.9985686&submitAction=ENTER&restaurantId=${id}`
+  );
   const restoData = data?.data;
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const [addedItems, setAddedItems] = useState([]);
   const [showSuccess, setShowSuccess] = useState(false);
-  console.log("24", data?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards)
+  console.log(
+    "24",
+    data?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
+      ?.card?.itemCards
+  );
   // ?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.categories
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
 
-  const filteredItems = ((data?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards) || []).filter((item) =>
+  const filteredItems = (
+    data?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
+      ?.card?.itemCards || []
+  ).filter((item) =>
     item?.card?.info?.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
   // console.log(filteredItems);
   const dispatch = useDispatch();
   const addFoodItem = (item) => {
-
     dispatch(addItem(item));
     setAddedItems([...addedItems, item?.card?.info?.id]);
-    console.log(addedItems)
+    console.log(addedItems);
     setShowSuccess(true);
-  }
+  };
   const isAddedToCart = (itemId) => {
     return addedItems.includes(itemId);
   };
@@ -63,40 +71,81 @@ function MenuDetails() {
       {data && (
         <>
           <div className="menu-details-banner">
-            <img className="menu-details-banner-image" src={IMG_BASE_URL + restoData?.cards[2]?.card?.card?.info?.cloudinaryImageId} alt={restoData.name} />
+            <img
+              className="menu-details-banner-image"
+              src={
+                IMG_BASE_URL +
+                restoData?.cards[2]?.card?.card?.info?.cloudinaryImageId
+              }
+              alt={restoData.name}
+            />
 
             <div className="menu-details-banner-info">
-              <h1 className="menu-details-banner-title">{restoData?.cards[0]?.card?.card?.text}</h1>
-              <h2 className="menu-details-banner-cuisines">{restoData?.cards[2]?.card?.card?.info?.cuisines.join(', ')}</h2>
-              <h2 className="menu-details-banner-location">{restoData?.cards[2]?.card?.card?.info?.city}, {restoData?.cards[0]?.card?.card?.info?.areaName}</h2>
+              <h1 className="menu-details-banner-title">
+                {restoData?.cards[0]?.card?.card?.text}
+              </h1>
+              <h2 className="menu-details-banner-cuisines">
+                {restoData?.cards[2]?.card?.card?.info?.cuisines.join(", ")}
+              </h2>
+              <h2 className="menu-details-banner-location">
+                {restoData?.cards[2]?.card?.card?.info?.city},{" "}
+                {restoData?.cards[0]?.card?.card?.info?.areaName}
+              </h2>
 
               <div className="menu-details-banner-down">
                 <div className="menu-details-banner-down-content">
                   {/* <p><i class="fa-solid fa-star" style={{ color: "white", fontSize: ".8rem", marginRight: ".3rem" }}></i>{restoData?.cards[2]?.card?.card?.info?.avgRating}</p> */}
-                  <p className={(restoData?.cards[2]?.card?.card?.info?.avgRating > 4 || random > 4) ? styles.infoItemTop : styles.infoItemPoor}><i className="fa-solid fa-star" style={{ color: "white", fontSize: ".7rem" }}></i> {(restoData?.cards[2]?.card?.card?.info?.avgRating > 4) ? restoData?.cards[2]?.card?.card?.info?.avgRating : random}</p>
+                  <p
+                    className={
+                      restoData?.cards[2]?.card?.card?.info?.avgRating > 4 ||
+                      random > 4
+                        ? styles.infoItemTop
+                        : styles.infoItemPoor
+                    }
+                  >
+                    <i
+                      className="fa-solid fa-star"
+                      style={{ color: "white", fontSize: ".7rem" }}
+                    ></i>{" "}
+                    {restoData?.cards[2]?.card?.card?.info?.avgRating > 4
+                      ? restoData?.cards[2]?.card?.card?.info?.avgRating
+                      : random}
+                  </p>
                 </div>
                 <div style={{ color: "gray" }}>|</div>
                 <div className="menu-details-banner-down-content">
-                  <p>{restoData?.cards[2]?.card?.card?.info?.costForTwoMessage}</p>
+                  <p>
+                    {restoData?.cards[2]?.card?.card?.info?.costForTwoMessage}
+                  </p>
                 </div>
               </div>
             </div>
 
             <div className="menu-details-banner-offers">
-              {restoData?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.offers.slice(0, 2).map((offer) => (
-                <div key={offer.id} className="menu-details-banner-offer">
-                  <img className="menu-details-banner-offer-image" src={discountLogo} alt="Offer" />
-                  <div className="menu-details-banner-offer-info">
-                    <h1 className="menu-details-banner-offer-header heading1">{offer?.info?.header}</h1>
-                    <h2 className="menu-details-banner-offer-code heading1">COUPON CODE: {offer?.info?.couponCode}</h2>
-                    <h2 className="menu-details-banner-offer-description heading1">APPLIES FOR: {offer?.info?.description}</h2>
+              {restoData?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.offers
+                .slice(0, 2)
+                .map((offer) => (
+                  <div key={offer.id} className="menu-details-banner-offer">
+                    <img
+                      className="menu-details-banner-offer-image"
+                      src={discountLogo}
+                      alt="Offer"
+                    />
+                    <div className="menu-details-banner-offer-info">
+                      <h1 className="menu-details-banner-offer-header heading1">
+                        {offer?.info?.header}
+                      </h1>
+                      <h2 className="menu-details-banner-offer-code heading1">
+                        COUPON CODE: {offer?.info?.couponCode}
+                      </h2>
+                      <h2 className="menu-details-banner-offer-description heading1">
+                        APPLIES FOR: {offer?.info?.description}
+                      </h2>
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
             </div>
           </div>
-
-
 
           <div className="menu-details-search">
             <input
@@ -107,30 +156,51 @@ function MenuDetails() {
             />
           </div>
 
-          <div className='menu-details-item-container'>
+          <div className="menu-details-item-container">
             {filteredItems.map((item) => {
-              const itemImg = item?.card?.info?.imageId ? IMG_BASE_URL + item?.card?.info?.imageId : foodPlaceholder;
+              const itemImg = item?.card?.info?.imageId
+                ? IMG_BASE_URL + item?.card?.info?.imageId
+                : foodPlaceholder;
+              const price =
+                item?.card?.info?.price ||
+                item?.card?.info?.defaultPrice ||
+                (Math.random() * (5 - 3) + 3).toFixed(1) * 100;
               return (
                 <div key={item?.card?.info?.id} className="menu-details-item">
-                  <h2 className="menu-details-item-name heading1">{item?.card?.info?.name}</h2>
-                  <img className="menu-details-item-image" src={itemImg} alt="menu-item" />
-                  <h2 className="menu-details-item-price heading1">RS: {(item?.card?.info?.price) / 100}/-</h2>
-                  {isAddedToCart(item?.card?.info?.id) ? <button className="menu-details-item-added-btn" disabled>Added To Cart</button> : <button className="menu-details-item-add-btn" onClick={() => addFoodItem(item)}>Add</button>}
+                  <h2 className="menu-details-item-name heading1">
+                    {item?.card?.info?.name}
+                  </h2>
+                  <img
+                    className="menu-details-item-image"
+                    src={itemImg}
+                    alt="menu-item"
+                  />
+                  <h2 className="menu-details-item-price heading1">
+                    RS: {price / 100}/-
+                  </h2>
+                  {isAddedToCart(item?.card?.info?.id) ? (
+                    <button className="menu-details-item-added-btn" disabled>
+                      Added To Cart
+                    </button>
+                  ) : (
+                    <button
+                      className="menu-details-item-add-btn"
+                      onClick={() => addFoodItem(item)}
+                    >
+                      Add
+                    </button>
+                  )}
                 </div>
               );
             })}
-            {showSuccess && <div className="popup">
-              Item added successfully!
-            </div>}
-
+            {showSuccess && (
+              <div className="popup">Item added successfully!</div>
+            )}
           </div>
-
-
         </>
       )}
     </div>
   );
 }
-
 
 export default MenuDetails;
